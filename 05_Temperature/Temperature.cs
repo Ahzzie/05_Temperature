9367b7e86afab6fe7b8fe314df484e39cc8848e9
Ahzzie <kulstof14@gmail.com>
2016-09-15 09:22:43 +0200

    FahrenheitToCelcius CelciusToFahrenheit done

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05_Temperature
+{
+    internal class Temperature
+    {
+        internal static double FahrenheitToCelcius(double f)
+        {
+            double c = (f - 32) * 5 / 9;
+
+            return c;
+        }
+
+        internal static double CelciusToFahrenheit(double c)
+        {
+            double f = c * 9/5 + 32;
+            return f;
+        }
+    }
+}
