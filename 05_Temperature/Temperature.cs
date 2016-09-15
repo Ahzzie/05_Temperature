bc6e68584a467667b63db2b4fcae27ff7f718cd8
Ahzzie <kulstof14@gmail.com>
2016-09-15 11:10:55 +0200

    done 15/09-16

--- 05_Temperature/Temperature.cs
+++ 05_Temperature/Temperature.cs
@@ -2,11 +2,39 @@
 
 namespace _05_Temperature
 {
+    public enum Unit
+    {
+        Fahrenheit,
+            Celcius
+    };
+ 
     internal class Temperature
     {
+        public Temperature(Unit unit, double input)
+        {
+            Fahrenheit = input;
+            Celcius = input; 
+        }
+
+        public Temperature()
+        { }
+        private double fahrenheit;
+        private double celcius;
+        public double Fahrenheit
+        {
+        get { return fahrenheit; }
+         set { fahrenheit = value; }
+        }
+
+        public double Celcius
+        {
+         get {return celcius;}
+         set {celcius = value;}
+        }
+
         internal static double FahrenheitToCelcius(double f)
         {
-            double c = (f - 32) * 5 / 9;
+            double c = (f - 32) * 5/9;
 
             return c;
         }
@@ -14,6 +42,7 @@ namespace _05_Temperature
         internal static double CelciusToFahrenheit(double c)
         {
             double f = c * 9/5 + 32;
+
             return f;
         }
     }
